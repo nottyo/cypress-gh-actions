277e1255c63822054e8fca50041abc012f7b6a1a
Traitanit Huangsri <fanmanutd@gmail.com>
2020-05-01 16:14:50 +0700

    add react todo tests (#1)
    * add react todo tests
    
    * fixed gh workflow
    
    * fixed gh workflow
    
    * fixed failed tests
    
    * fixed workflow
    
    * add missing files
    
    Co-authored-by: traitanit-h <traitanit.h@linecorp.com>

--- cypress/integration/todo_react.spec.ts
+++ cypress/integration/todo_react.spec.ts
@@ -1,14 +1,23 @@
 import locators from '../fixtures/locators';
+const reactTodo = 'http://todomvc.com/examples/react/'
 
 describe('todo react test', () => {
 
   beforeEach(() => {
-    cy.visit(Cypress.config('baseUrl') as string);
+    cy.visit(reactTodo);
   });
 
   it('can add new todo', () => {
     const todo = 'Test React Todo with Cypress';
-    cy.get(locators.newTodoInput).type(`${todo}{enter}`);
-    cy.get(locators.todoLabel).first().should('have.text', todo);
+    cy.get(locators.reactTodo.newTodoInput).type(`${todo}{enter}`);
+    cy.get(locators.reactTodo.todoLabel).first().should('have.text', todo);
   });
+
+  it('should be able to delete todo', () => {
+    const todo = 'will be deleted'
+    cy.get(locators.reactTodo.newTodoInput).type(`${todo}{enter}`);
+    cy.get(locators.reactTodo.toggleAll).click();
+    cy.get(locators.reactTodo.clearCompleted).click();
+    cy.get(locators.reactTodo.todoList).should('not.be.visible')
+  })
 });

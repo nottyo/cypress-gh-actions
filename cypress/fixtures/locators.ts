e24bb72002a1f0385404340861e38fd522f36f54
traitanit-h <traitanit.h@linecorp.com>
2020-05-01 15:19:08 +0700

    add react todo tests

--- cypress/fixtures/locators.ts
+++ cypress/fixtures/locators.ts
@@ -1,6 +1,15 @@
 const locators = {
-  newTodoInput: '.new-todo',
-  todoLabel: '.todo > .view > label',
+  vueTodo: {
+    newTodoInput: '.new-todo',
+    todoLabel: '.todo > .view > label',
+  },
+  reactTodo: {
+    newTodoInput: '.new-todo',
+    todoLabel: 'div[class="view"] > label',
+    toggleAll: 'input[class="toggle"]',
+    clearCompleted: 'button[class="clear-completed"]',
+    todoList: 'ul[class="todo-list"]',
+  }
 }
 
 export default locators;
--- cypress/integration/todo_vue.spec.ts
+++ cypress/integration/todo_vue.spec.ts
@@ -8,12 +8,12 @@ describe('todo vue test', () => {
 
   it('can add new todo', () => {
     const todo = 'Test Vue Todo with Cypress';
-    cy.get(locators.newTodoInput).type(`${todo}{enter}`);
-    cy.get(locators.todoLabel).first().should('have.text', todo);
+    cy.get(locators.vueTodo.newTodoInput).type(`${todo}{enter}`);
+    cy.get(locators.vueTodo.todoLabel).first().should('have.text', todo);
   });
 
   it('always failed this test', () => {
     const expectPlaceHolders = 'My Todos'
-    cy.get(locators.newTodoInput).should('have.attr', 'placeholder', expectPlaceHolders);
+    cy.get(locators.vueTodo.newTodoInput).should('have.attr', 'placeholder', expectPlaceHolders);
   })
 });
